6505b5ffa45abe18b816abd944d4c82d22009739
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-02-15 10:22:05 +0700

    chore: Remove OpenCL ROCm as it's currently not needed
    This commit removes the OpenCL ROCm implementation from the NixOS configuration.
    It's currently not required, simplifying the configuration and reducing unnecessary overhead.
    
    If OpenCL ROCm becomes necessary again in the future, it can be easily re-added. :3

--- nixos/drivers.nix
+++ nixos/drivers.nix
@@ -1,8 +1,4 @@
 { config, lib, pkgs, ... }: {
     boot.initrd.kernelModules = [ "amdgpu" ];
     hardware.graphics.enable32Bit = true; # For 32 bit applications
-
-    hardware.opengl.extraPackages = with pkgs; [
-        rocmPackages.clr.icd
-    ];
 }
