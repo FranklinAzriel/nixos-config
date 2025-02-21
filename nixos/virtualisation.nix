b73e2b2a36c7bbab0a2f68c55282e6f4cff137ec
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-02-21 16:45:12 +0700

    feat: Configure virtualization environment with libvirt and virt-manager
    This commit sets up the virtualization environment using libvirt and
    virt-manager.  This allows developers to easily create and manage
    virtual machines for testing and development.

---
+++ nixos/virtualisation.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }: {
+    virtualisation.libvirtd = {
+        enable = true;
+        qemu = {
+            package = pkgs.qemu_kvm;
+            runAsRoot = true;
+            swtpm.enable = true;
+            ovmf.enable = true;
+        };
+    };
+
+    environment.systemPackages = with pkgs; [
+        virt-manager
+    ];
+}
