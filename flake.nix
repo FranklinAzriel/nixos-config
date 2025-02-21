a6a96c11acbebf2010dddc2ea25da3f57f4d3e6e
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-02-21 16:40:00 +0700

    chore: Add Chaotic and Lanzaboote flakes and update dependencies
    This commit adds the Chaotic and Lanzaboote flakes and also updates
    other dependencies in the project.

--- flake.nix
+++ flake.nix
@@ -3,18 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    chaotic = {
+      url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, chaotic, lanzaboote, ... }: {
     nixosConfigurations = {
       Holy-Nix = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./nixos/configuration.nix
+
+          lanzaboote.nixosModules.lanzaboote
+
+          chaotic.nixosModules.nyx-cache
+          chaotic.nixosModules.nyx-overlay
+          chaotic.nixosModules.nyx-registry
+
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
