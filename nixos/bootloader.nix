320746b21af2b453d5cf41688843f9619fcb7bc5
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-02-21 16:43:52 +0700

    feat: Enable Secure Boot with user-signed keys via Lanzaboote
    This commit switches the boot process to Lanzaboote to enable Secure Boot.
    User-signed keys are now used for UEFI verification.

--- nixos/bootloader.nix
+++ nixos/bootloader.nix
@@ -1,11 +1,24 @@
 { config, lib, pkgs, ... }: {
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  # Lanzaboote currently replaces the systemd-boot module.
+  # This setting is usually set to true in configuration.nix
+  # generated at installation time. So we force it to false
+  # for now.
+  boot.loader.systemd-boot.enable = lib.mkForce false;
 
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+  
   boot.loader = {
     efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot/efi";
     };
   };
+
+  environment.systemPackages = [
+    # For debugging and troubleshooting Secure Boot.
+    pkgs.sbctl
+  ];
 }
