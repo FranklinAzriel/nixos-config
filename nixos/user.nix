983d0da68f720a41aeb4d8e2520330d58d2bc9c3
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-02-22 21:12:07 +0700

    feat: Configure libvirt user and install Steam with dependencies
    This commit adds a dedicated user for libvirt VM management.
    It also installs Steam and its necessary dependencies for gaming.

--- nixos/user.nix
+++ nixos/user.nix
@@ -2,6 +2,13 @@
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.FranklinAzriel = {
         isNormalUser = true;
-        extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+        extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
+    };
+
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
 }
