89996d738c604a0a18e531a792a39b552eccac6d
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-02-22 21:16:07 +0700

    feat: Add multimedia and gaming packages via Home Manager
    This commit adds OBS, VLC, and PrismLauncher to the user's packages
    managed by Home Manager.

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,12 +1,24 @@
 { config, pkgs, ... }: {
     home.packages = with pkgs; [
+
+        # Browser
         google-chrome
 
+        # Social Media
         telegram-desktop
         discord
 
+        # Development
         git
         gnupg
 	    vscode
+
+        # Multimedia
+        vlc
+        obs-studio
+
+        # Games
+        prismlauncher
+
     ];
 }
