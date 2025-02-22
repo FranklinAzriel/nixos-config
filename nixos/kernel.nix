5d9385179834c5c704cf48559ccef38da79fd2a9
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-02-22 21:15:03 +0700

    feat: Enable nested KVM and optimize system with CachyOS udev rules
    This commit enables AMD nested KVM via modprobe and adds CachyOS udev
    rules for performance and permissions.  Specifically:
    
    - Enables nested KVM for AMD processors through modprobe configuration.
    - Sets ZRAM swappiness to a more aggressive value for improved caching.
    - Grants access to rtc0 and hpet device nodes to the audio group.
    - Selects optimal I/O schedulers for HDD, SSD, and NVMe drives.
    - Grants access to the cpu_dma_latency device node to the audio group.
    
    These changes are intended to improve virtualization capabilities,
    system performance, and device access.

---
+++ nixos/kernel.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }: {
+    
+    boot.extraModprobeConfig = ''
+        options kvm_amd nested=1
+
+        options rtw89_pci disable_aspm_l1=y disable_aspm_l1ss=y
+        options rtw89pci disable_aspm_l1=y disable_aspm_l1ss=y
+        options rtw89_core disable_ps_mode=y
+        options rtw89core disable_ps_mode=y
+    '';
+
+    services.udev.extraRules = ''
+        TEST!="/dev/zram0", GOTO="zram_end"
+
+        # When used with ZRAM, it is better to prefer page out only anonymous pages,
+        # because it ensures that they do not go out of memory, but will be just
+        # compressed. If we do frequent flushing of file pages, that increases the
+        # percentage of page cache misses, which in the long term gives additional
+        # cycles to re-read the same data from disk that was previously in page cache.
+        # This is the reason why it is recommended to use high values from 100 to keep
+        # the page cache as hermetic as possible, because otherwise it is "expensive"
+        # to read data from disk again. At the same time, uncompressing pages from ZRAM
+        # is not as expensive and is usually very fast on modern CPUs.
+        SYSCTL{vm.swappiness}="150"
+
+        LABEL="zram_end"
+
+
+        KERNEL=="rtc0", GROUP="audio"
+        KERNEL=="hpet", GROUP="audio"
+
+        # HDD
+        ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", \
+            ATTR{queue/scheduler}="bfq"
+
+        # SSD
+        ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", \
+            ATTR{queue/scheduler}="mq-deadline"
+
+        # NVMe SSD
+        ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", \
+            ATTR{queue/scheduler}="none"
+
+        DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+        
+    '';
+}
