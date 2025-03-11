2e6dbcedc154f5300cc688bac241534aa0ac0fa3
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-03-11 23:52:42 +0700

    fix: Update GPG key
    Updated the GPG key used for Git signing.

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -5,6 +5,6 @@
         userName = "FranklinAzriel";
         userEmail = "FranklinAzriel6942@gmail.com";
         signing.signByDefault = true;
-        signing.key = "0xCE02984C1DD7D4CD";
+        signing.key = "0xDD35555717C0F0AD";
     };
 }
