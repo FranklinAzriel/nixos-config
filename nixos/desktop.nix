1266656f77bcb0abac6bb89ce88b9e8206044257
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-03-11 23:53:41 +0700

    feat: Enable Bluetooth and MPRIS proxy for media control
    This commit enables Bluetooth support and configures the MPRIS proxy service.
    
    - Enabled `hardware.bluetooth.enable` to activate Bluetooth functionality.
    - Set `hardware.bluetooth.settings.General.Experimental` to `true` to enable experimental Bluetooth features (if needed).
    - Added a systemd user service `mpris-proxy` which:
      - Starts the `mpris-proxy` executable from the BlueZ package.
      - Depends on `network.target` and `sound.target` to ensure network and audio are available.
      - Is wanted by `default.target`, ensuring it runs in the user's session.
    
    These changes allow for controlling media playback (e.g., play/pause, next/previous) using Bluetooth buttons that support the MPRIS standard.

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -8,10 +8,21 @@
     services.pipewire = {
         enable = true;
         pulse.enable = true;
+        alsa.enable = true;
+        jack.enable = true;
+        alsa.support32Bit = true;
+    };
+    
+    services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
     };
 
     environment.systemPackages = with pkgs; [
       kdePackages.plasma-vault
-
     ];
 }
--- nixos/network.nix
+++ nixos/network.nix
@@ -1,4 +1,16 @@
 { config, lib, pkgs, ... }: {
     networking.hostName = "Holy-Nix"; # Define your hostname.
     networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    
+    hardware.bluetooth = {
+        enable = true; # enables support for Bluetooth
+        settings.General.Experimental = true;
+    };
+
+    systemd.user.services.mpris-proxy = {
+        description = "Mpris proxy";
+        after = [ "network.target" "sound.target" ];
+        wantedBy = [ "default.target" ];
+        serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+    };
 }
