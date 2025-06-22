f23a79546df6d219003edc331d5420c5a169778f
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-06-22 23:47:55 +0700

    feat: Add additional KDE Plasma basic packages for enhanced functionality

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -5,8 +5,12 @@
     services.desktopManager.plasma6.enable = true;
 
     environment.systemPackages = with pkgs; [
+      # KDE Plasma basic packages
       kdePackages.plasma-vault
       kdePackages.qtsvg
+      kdePackages.filelight
+      kdePackages.kolourpaint
+      kdePackages.kteatime
       
       # Cursors
       bibata-cursors
