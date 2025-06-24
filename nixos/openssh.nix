131cb63e0534a9a02fe28315ceadad0dda18a2d5
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-06-24 09:43:34 +0700

    feat: Update OpenSSH configuration with additional security settings

--- nixos/openssh.nix
+++ nixos/openssh.nix
@@ -8,6 +8,11 @@
         UseDns = false;
         X11Forwarding = false;
         PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
-    };
+        AllowTcpForwarding = "no"; # "yes", "no", "local", "remote", "all"
+        ClientAliveCountMax = 1; # Number of client alive messages before disconnecting
+        MaxAuthTries = 3; # Maximum number of authentication attempts per connection
+        MaxSessions = 1; # Maximum number of open sessions per connection
+        LogLevel = "VERBOSE"; # "QUIET", "FATAL", "ERROR", "INFO", "VERBOSE", "DEBUG", "DEBUG1", "DEBUG2", "DEBUG3"
+        };
     };
 }
