ba68b5f186679e623143e02cdfd483384b96c567
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-06-27 15:10:01 +0700

    feat: Add additional build tools to systemPackages in packages.nix

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -3,5 +3,17 @@
         pkgs.file
         pkgs.pciutils
         pkgs.cachix
+
+        # Build tools
+        pkgs.gcc
+        pkgs.binutils
+        pkgs.cmake
+        pkgs.gnumake
+        pkgs.clang
+
+        # 
+        pkgs.glib
+        pkgs.glibc
+    
     ];
 }
