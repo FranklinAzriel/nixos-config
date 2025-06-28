d0e30b8f23a975a23ffe029af37d5460bb8553ff
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-06-28 14:23:50 +0700

    feat: enhance comments in openssh.nix for better clarity

--- nixos/openssh.nix
+++ nixos/openssh.nix
@@ -1,18 +1,20 @@
 { config, lib, pkgs, ... }: {
+
+    # OpenSSH configuration.
     services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings = {
-        PasswordAuthentication = false;
-        AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-        UseDns = false;
-        X11Forwarding = false;
-        PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
-        AllowTcpForwarding = "no"; # "yes", "no", "local", "remote", "all"
-        ClientAliveCountMax = 1; # Number of client alive messages before disconnecting
-        MaxAuthTries = 3; # Maximum number of authentication attempts per connection
-        MaxSessions = 1; # Maximum number of open sessions per connection
-        LogLevel = "VERBOSE"; # "QUIET", "FATAL", "ERROR", "INFO", "VERBOSE", "DEBUG", "DEBUG1", "DEBUG2", "DEBUG3"
+        enable = true; # Enable OpenSSH server
+        ports = [ 22 ];
+        settings = {
+            PasswordAuthentication = false; # Disabled for security, use SSH keys instead.
+            AllowUsers = null; # Allows all users by default.
+            UseDns = false;
+            X11Forwarding = false;
+            PermitRootLogin = "no"; # Disabled for security, root login via SSH is not recommended.
+            AllowTcpForwarding = "no";
+            ClientAliveCountMax = 1; # Number of client alive messages before disconnecting
+            MaxAuthTries = 3; # Maximum number of authentication attempts per connection
+            MaxSessions = 1; # Maximum number of open sessions per connection
+            LogLevel = "VERBOSE"; # Needed by fail2ban to log SSH events.
         };
     };
 }
