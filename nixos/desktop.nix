06085f13c46d0f13159fc1fd0e1ca40ca2d94090
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-06-28 14:10:01 +0700

    feat: improve comments in desktop.nix for better clarity

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -1,9 +1,13 @@
 { config, lib, pkgs, ... }: {
+
     # Enable the X11 windowing system.
     services.xserver.enable = true;
+
+    # Enable the KDE Plasma 6 desktop environment.
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
 
+    # Needed packages for KDE Plasma 6.
     environment.systemPackages = with pkgs; [
       # KDE Plasma basic packages
       kdePackages.plasma-vault
