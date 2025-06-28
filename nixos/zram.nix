93752356ab34f88272ceeaa70dbae28f585bc0ef
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-06-28 14:25:03 +0700

    feat: enhance comments in zram.nix for better clarity

--- nixos/zram.nix
+++ nixos/zram.nix
@@ -1,10 +1,11 @@
 { config, lib, pkgs, ... }: {
+
     services.zram-generator = {
-        enable = true;
+        enable = true; # Enable zram-generator service
         settings.zram0 = {
             compression-algorithm = "zstd lz4 (type=huge)";
-            zram-size = "ram * 3 / 4";
-            swap-priority = 32767;
+            zram-size = "ram * 3 / 4"; # Use 75% of RAM for zram
+            swap-priority = 32767; # Set high priority for zram before other swap devices.
             fs-type = "swap";
         };
     };
