f8127c1175c1b7e9fcc12a77d74f9306b6d46060
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-06-29 07:40:08 +0700

    refactor: reorder Steam configuration for improved clarity

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -1,11 +1,12 @@
 { config, lib, pkgs, ... }: {
+
     programs.steam = {
-            enable = true;
-            gamescopeSession.enable = true; 
-            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-            localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-        };
+        enable = true; # Enable Steam
+        gamescopeSession.enable = true; 
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
     
     programs.gamemode.enable = true;
     programs.gamescope.enable = true;
