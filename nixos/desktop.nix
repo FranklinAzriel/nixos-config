1baebe8c0563f14c0e2147ec61674ee9979b8733
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-10-15 16:52:17 +0700

    feat: enhance desktop environment configuration and added some packages
    This update adds support for managing system fonts and icons through a new file system binding mechanism. It introduces the `mkRoSymBind` function to create read-only symbolic bindings for font and icon directories, ensuring a cleaner and more organized setup. Additionally, the input method configuration for `fcitx5` is included, enabling better language support for users.

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -16,10 +16,46 @@
       kdePackages.kolourpaint
       kdePackages.kteatime
 
+      # 
+      kdePackages.partitionmanager
+      pkgs.kdiskmark
+
       # Virtual desktops
       pkgs.kara
       
       # Cursors
       bibata-cursors
     ];
+    system.fsPackages = [ pkgs.bindfs ];
+
+  fileSystems = let
+    mkRoSymBind = path: {
+      device = path;
+      fsType = "fuse.bindfs";
+      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+    };
+    aggregated = pkgs.buildEnv {
+        name = "system-fonts-and-icons";
+        paths = config.fonts.packages ++ (with pkgs; [
+          # Add your cursor themes and icon packages here
+          bibata-cursors
+          # etc.
+        ]);
+        pathsToLink = [ "/share/fonts" "/share/icons" ];
+    };
+  in {
+    "/usr/share/fonts" = mkRoSymBind "${aggregated}/share/fonts";
+    "/usr/share/icons" = mkRoSymBind "${aggregated}/share/icons";
+  };
+
+  i18n.inputMethod = {
+   type = "fcitx5";
+   enable = true;
+   fcitx5.addons = with pkgs; [
+     fcitx5-mozc
+     fcitx5-gtk
+   ];
+ };
+
+
 }
