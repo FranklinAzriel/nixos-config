b5554b269a7e99283c73a3917161a55a51d2c35c
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-10-15 16:43:22 +0700

    fix: adjust zram swap priority

--- nixos/zram.nix
+++ nixos/zram.nix
@@ -5,7 +5,7 @@
         settings.zram0 = {
             compression-algorithm = "zstd lz4 (type=huge)";
             zram-size = "ram * 3 / 4"; # Use 75% of RAM for zram
-            swap-priority = 32767; # Set high priority for zram before other swap devices.
+            swap-priority = 100; # Set high priority for zram before other swap devices.
             fs-type = "swap";
         };
     };
