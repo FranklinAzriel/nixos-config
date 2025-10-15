86bb5233d64fd17d7e44b3e1af4127de120d645b
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-10-15 16:52:33 +0700

    feat: add Chromium configuration to home-manager
    This adds a configuration for the Chromium browser in the home-manager setup, enabling it and specifying the package to use. Additionally, it sets desired launch arguments to enhance video decoding and encoding capabilities. This change aims to improve the user experience by ensuring that Chromium is properly configured for optimal performance.

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,38 +1,70 @@
 { config, pkgs, ... }: {
+    programs.chromium = {
+    enable = true;
+    # Ensure you are using the correct package for Chrome Stable
+    # This is typically 'google-chrome' or 'google-chrome-stable'
+    package = pkgs.google-chrome;
+
+    # Set your desired launch arguments as a list of strings
+    commandLineArgs = [
+      "--enable-features=VaapiVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiVideoEncoder,Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport,UseMultiPlaneFormatForHardwareVideo"
+    ];
+  };
+
     home.packages = with pkgs; [
 	
 	    # Shell
 	    fzf
-        fastfetch
-        
-        # Browser
-        google-chrome
+      fastfetch
 
-        # Social Media
-        telegram-desktop
-        discord
+      # Browser
+      #google-chrome
 
-        # Development
-        git
-        gnupg
-        postman
-        dbeaver-bin
-        
-        # IDE
+      # Social Media
+      telegram-desktop
+      discord
+      thunderbird
+
+      # Development
+      git
+      uv
+      gnupg
+      postman
+      #dbeaver-bin
+      python3
+      python3Packages.pip
+      virtualenv
+      nodejs
+      nodePackages.npm
+
+      kubectl
+      kubernetes-helm
+      # IDE
 	    vscode
-        jetbrains.idea-community-bin
+      #jetbrains.idea-community-bin
+      
+      # Multimedia
+      vlc
+      obs-studio
 
-        # Multimedia
-        vlc
-        obs-studio
+      # Games
+      (prismlauncher.override {
+        jdks = [
+          temurin-jre-bin-8
+          temurin-jre-bin
+        ];
+      })
 
-        # Games
-        prismlauncher
+      # Tools
+      qdiskinfo        
 
-        # Tools
-        qdiskinfo
-        
 	    # Password Manager
 	    bitwarden-desktop
+
+      # Torrent
+      qbittorrent
+      transmission_4
+
+      playerctl
     ];
 }
