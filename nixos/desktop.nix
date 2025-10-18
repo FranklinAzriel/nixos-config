160a64618e7728fa953f4d2b839e89b61f714d08
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-10-18 15:21:18 +0700

    refactor: reorganize desktop configuration for clarity

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -1,33 +1,51 @@
 { config, lib, pkgs, ... }: {
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+      settings = {
+        General.DisplayServer = "wayland";
+      };
+    }; 
+  };
 
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
+  # Enable the KDE Plasma 6 desktop environment.
+  services.desktopManager.plasma6.enable = true;
 
-    # Enable the KDE Plasma 6 desktop environment.
-    services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
+  # Needed packages for KDE Plasma 6.
+  environment.systemPackages = with pkgs; [
+    # KDE Plasma basic packages
+    kdePackages.plasma-desktop
+    kdePackages.plasma-vault
+    kdePackages.qtsvg
+    kdePackages.filelight
+    kdePackages.kolourpaint
+    kdePackages.kteatime
 
-    # Needed packages for KDE Plasma 6.
-    environment.systemPackages = with pkgs; [
-      # KDE Plasma basic packages
-      kdePackages.plasma-vault
-      kdePackages.qtsvg
-      kdePackages.filelight
-      kdePackages.kolourpaint
-      kdePackages.kteatime
+    # KDE Plasma partition tools
+    kdePackages.partitionmanager
+    kdiskmark
 
-      # 
-      kdePackages.partitionmanager
-      pkgs.kdiskmark
+    # Virtual desktops
+    kara
 
-      # Virtual desktops
-      pkgs.kara
-      
-      # Cursors
-      bibata-cursors
-    ];
-    system.fsPackages = [ pkgs.bindfs ];
+    # Cursors
+    bibata-cursors
+  ];
+    
+  i18n.inputMethod = {
+   type = "fcitx5";
+   enable = true;
+   fcitx5.addons = with pkgs; [
+     fcitx5-mozc
+     fcitx5-gtk
+   ];
+ };
 
+  system.fsPackages = [ pkgs.bindfs ];
   fileSystems = let
     mkRoSymBind = path: {
       device = path;
@@ -47,15 +65,4 @@
     "/usr/share/fonts" = mkRoSymBind "${aggregated}/share/fonts";
     "/usr/share/icons" = mkRoSymBind "${aggregated}/share/icons";
   };
-
-  i18n.inputMethod = {
-   type = "fcitx5";
-   enable = true;
-   fcitx5.addons = with pkgs; [
-     fcitx5-mozc
-     fcitx5-gtk
-   ];
- };
-
-
 }
