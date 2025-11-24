75388df71f9dea5086fa697f486444b416f0af8d
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-11-24 21:36:14 +0700

    feat(tailscale): Disable Tailscale DNS.
    This changes is needed to prevent Tailscale from overriding the systemd-resolved DNS settings, which were causing issues with DNS resolution on the system.

--- nixos/laptop/connectivity/tailscale.nix
+++ nixos/laptop/connectivity/tailscale.nix
@@ -1,3 +1,10 @@
 { pkgs, ... }: {
-    services.tailscale.enable = true; # Enable Tailscale.
+    services.tailscale = {
+        enable = true; # Enable Tailscale.
+        authKeyFile = "/root/tailscale_key";
+        extraUpFlags = [
+            "--accept-dns=false"
+        ]; 
+    };
+
 }
