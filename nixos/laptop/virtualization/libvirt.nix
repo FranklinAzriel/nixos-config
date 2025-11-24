6f43807b90277ee84f1dd57174a8e1e2ad3e4299
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-11-24 21:32:01 +0700

    feat(libvirt): Change qemu config to use qemu_kvm instead of qemu_full
    This change updates the libvirt configuration to utilize qemu_kvm, since other architectures are not needed yet. This should decrease disk usage.

--- nixos/laptop/virtualization/libvirt.nix
+++ nixos/laptop/virtualization/libvirt.nix
@@ -6,16 +6,17 @@
             enable = true; # 
             enableGuest = true;
         };
-        #qemu = {
-            #package = pkgs.qemu_full;
-            #runAsRoot = true;
-            #swtpm.enable = true;
-            #vhostUserPackages = with pkgs; [ virtiofsd ];
-        #};
+        qemu = {
+            package = pkgs.qemu_kvm;
+            runAsRoot = true;
+            swtpm.enable = true;
+            vhostUserPackages = with pkgs; [ virtiofsd ];
+        };
     };
 
     environment.systemPackages = with pkgs; [
         virt-manager # View and manage virtual machines
     ];
-    
+
+    virtualisation.spiceUSBRedirection.enable = true;
 }
