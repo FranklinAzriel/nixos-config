c9022633424b06679d9321cae2e83974ac95b4b8
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-11-27 19:01:32 +0700

    feat(tailscale): Move tailscale config to common connectivity
    Moved the tailscale configuration from the laptop profile to the common connectivity

--- nixos/common/connectivity/default.nix
+++ nixos/common/connectivity/default.nix
@@ -2,5 +2,6 @@
     imports = [
         ./bluetooth.nix
         ./networkmanager.nix
+        ./tailscale.nix
     ];
 }
--- nixos/laptop/connectivity/default.nix
+++ nixos/laptop/connectivity/default.nix
@@ -1,6 +1,5 @@
 { ... }: {
     imports = [
         ./hostname.nix
-        ./tailscale.nix
     ];
 }
