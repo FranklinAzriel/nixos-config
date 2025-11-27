b052ce85ec8ecc0a35c65ea344e2165e508390a9
FranklinAzriel <FranklinAzriel6942@gmail.com>
2025-11-27 19:33:50 +0700

    feat(openssh): Add additional SSH authorized key for root
    This change adds a new SSH authorized key for the root user in the
    OpenSSH configuration. This allows for an additional method of secure
    access to the homelab environment, enhancing flexibility in user
    authentication.

--- nixos/homelab/openssh.nix
+++ nixos/homelab/openssh.nix
@@ -9,6 +9,7 @@
 
     users.users.root.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILl50pmugL84aI4kD038ls1NKgyDAv5oERwESf3SAYZT Trusted Key"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMW+CIdlFb1QrvjxNQqI/jbbU7Ity6/lvaeMmoeJP0pV Holy-Nix"
     ];
 
 }
